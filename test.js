4f97534f7151b9442aef7fa382744931964108f2
Anwesh-18 <cse.24bcsh15@silicon.ac.in>
2025-10-02 15:34:57 +0530

    data types in js

--- test.js
+++ test.js
@@ -3,3 +3,14 @@ let gmail = "Anwesh@example.com";
 // console.table([name, gmail]);
 let price = 21.34;
 console.log(typeof price);
+
+/*
+number => 2 to the power 53
+bigint => 2 to the power 53
+string => "Anwesh"  
+boolean => true/false
+undefined => variable not assigned
+null => empty value
+symbol => unique value
+object => array, function, date, regex
+*/
