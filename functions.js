41265195cbe7131c181d2cbb9f5432057b419194
Anwesh-18 <cse.24bcsh15@silicon.ac.in>
2025-10-04 19:46:29 +0530

    learned execution and its structure

--- functions.js
+++ functions.js
@@ -16,3 +16,19 @@
 
 // handleObject(user);
 
+// (function connect(){ // named iife
+//     console.log("Connected to the database");
+// })(); // IIFE - Immediately Invoked Function Expression
+// // the semicolon at the end is important here to avoid issues if this code is concatenated with other scripts without semicolons.
+
+// (() => { // unnamed iife
+//     console.log("This is an arrow function IIFE");
+// })(); // Arrow function IIFE
+
+// // Arrow functions
+// const add = (a, b) => a + b;
+// console.log(add(2, 3));
+
+// JavaScript execution context
+// 1. Global Execution Context
+// 2. Function Execution Context
