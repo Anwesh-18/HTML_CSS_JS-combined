8ca3557ced539363befb772dfd2cbed189bd00c7
Anwesh-18 <cse.24bcsh15@silicon.ac.in>
2025-10-04 18:29:37 +0530

    learned functions and scoping

---
+++ functions.js
@@ -0,0 +1,18 @@
+// function sayMyname(name) {
+//     console.log(`Hello ${name}`);   
+// }
+// sayMyname("Anwesh");
+
+// const user = {
+//     name: "Anwesh",
+//     age: 20,
+// }
+
+// function handleObject(obj) {
+//     for (let key in obj) {
+//         console.log(`${key}: ${obj[key]}`);
+//     }
+// }
+
+// handleObject(user);
+
