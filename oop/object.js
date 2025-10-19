bba4dbce2f1a8a95e1356cdd85ed5e5bd2d35cf0
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-10-19 20:18:09 +0530

    object creation

---
+++ oop/object.js
@@ -0,0 +1,35 @@
+// function multiplyByFive(num,score){
+//     this.num = num
+//     this.score = score
+// }
+
+// const { use } = require("react")
+
+// multiplyByFive.prototype.power2 = function(num){
+//     return num*num;
+// }
+
+// // console.log();
+// // console.log(multiplyByFive.power);
+// const ob = new multiplyByFive(5,10); // it gives constructor functions with new key word
+// console.log(ob.power2(10));
+// console.log(ob.prototype);
+
+// proper use of new
+const userCreate = function(userName,pass){
+    this.userName = userName
+    this.pass = pass
+}
+
+userCreate.prototype.changePass = function(newPass){
+    this.pass = newPass;
+}
+
+// creating an onbject..new keyword is neccessary to update the object about the newly created methods!!
+const user = new userCreate("Anwesh","Anwesh123")
+console.log(`old Pass: ${user.pass}`)
+
+// using my self created function/method
+user.changePass("Anwesh007")
+console.log(`new Pass: ${user.pass}`);
+
--- oop/oop.js
+++ oop/oop.js
@@ -10,3 +10,18 @@ const user = {
     }
 }
 console.log(user.getUserDetails());
+
+// constructor function
+const User = function(userName,userPass,isLoggedin){
+    this.userName = userName;
+    this.userPass = userPass;
+    this.isLoggedin = isLoggedin;
+
+    return this;
+}
+
+const user1 = new User("Anwesh","Anwesh123",true)
+const user2 = new User("Hitesh","Hitesh789",false)
+
+console.log(user1)
+console.log(user2)
