e95d941b04ec9dba556eb65aa28edf15fb9ecbd8
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-10-19 21:54:15 +0530

    inheritance of prototype

---
+++ oop/prototype.js
@@ -0,0 +1,55 @@
+// let myname = "Anwesh   "
+// console.log(trueLength(myname));
+
+// // I want a method true length that ignore the spaces
+// String.prototype.trueLength = function(str){
+//     return str.trim()
+// }
+
+let myHeros = ["Thor","hulk"]
+
+let heroPower = {
+    thor: "hammer",
+    hulk: "Gamma",
+
+    getHulkPower: function(){
+        console.log("Got Gamma power!!");
+    }
+}
+
+Object.prototype.anwesh = function(){
+    console.log("anwesh is present in all objects");
+}
+
+// heroPower.anwesh()
+// myHeros.anwesh()
+
+Array.prototype.anwesh_2 = function(){
+    console.log("power injected in array");   
+}
+myHeros.anwesh_2()
+// heroPower.anwesh_2()
+
+const User = {
+    name: "Anwesh";
+    Email: "Anwesh@example.com"
+}
+
+const Teacher = {
+    isYoutuber: false,
+    isAvailable: true
+}
+
+const TeachingSupport = {
+    isGood: true,
+    isAvailable: true
+}
+
+const TeacherAssistant = {
+    isAvailable: false,
+    isProfessional: true,
+    __proto__: TeachingSupport // we can assign the prototype inside
+}
+Teacher.__proto__ = User // we can also assign the prototype outside
+
+Object.setPrototypeOf(TeachingSupport,Teacher) //we can use setPrototypeOf method as well
