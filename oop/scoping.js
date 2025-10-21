70cabfdbc16599523428c3d4b67c57bb3c404bf5
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-10-21 11:41:51 +0530

    js completed

--- oop/scoping.js
+++ oop/scoping.js
@@ -1,28 +1,39 @@
-function outer(){
-    let myName = "Anwesh"
-    // console.log(child); // can not access it here
-    function inner(){
-        let child = "child";
-        console.log("inner",myName);
-    }
-    function innerTwo(){
-        console.log("innerTwo",myName);
-        // console.log(child); // can not access it here as well
-    }
-    inner()
-    innerTwo()
-}
-outer()
-// console.log(myName);
+// function outer(){
+//     let myName = "Anwesh"
+//     // console.log(child); // can not access it here
+//     function inner(){
+//         let child = "child";
+//         console.log("inner",myName);
+//     }
+//     function innerTwo(){
+//         console.log("innerTwo",myName);
+//         // console.log(child); // can not access it here as well
+//     }
+//     inner()
+//     innerTwo()
+// }
+// outer()
+// // console.log(myName);
 
-// closure
-function makeFunc(){
-    const name = 'myname'
-    function displayName(){
-        console.log(name);
-    }
-    return displayName; // this sends the whole lexical scope to the calling var
-}
-const myFunc = makeFunc()
-myFunc()
+// // closure
+// function makeFunc(){
+//     const name = 'myname'
+//     function displayName(){
+//         console.log(name);
+//     }
+//     return displayName; // this sends the whole lexical scope to the calling var
+// }
+// const myFunc = makeFunc()
+// myFunc()
 
+// Its real life use
+function clickHandler(color){
+    // document.body.style.backgroundColor = color
+    function setColor(){
+        document.body.style.backgroundColor = color
+    }
+    return setColor // this passes the referrence of the whole scope
+}
+
+document.getElementById('orange').onclick = clickHandler('orange')
+document.getElementById('green').onclick = clickHandler('green')
